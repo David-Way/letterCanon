0f161f446c3d49ee555c13b0dca2a079e1922657
David-Way <david_way@live.ie>
2015-12-27 01:37:02 +0000

    code clean up, added function for calculating character X position

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,8 @@ var webserver = require('gulp-webserver');
 gulp.task('serve', function() {
   gulp.src('./')
     .pipe(webserver({
+      host: '0.0.0.0',
+      port: 8000,
       livereload: true,
       directoryListing: true,
       open: true
